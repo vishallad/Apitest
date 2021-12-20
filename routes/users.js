f5501bd79a243f7a5cd45dbf67b53735923d86bc
vikram shah <vikram.shah@tiez.nl>
2021-12-20 23:28:19 +0530

    first commit

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var users = require("../data/users")
+
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.json(users);
+});
+
+module.exports = router;
